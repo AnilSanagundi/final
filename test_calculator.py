8bbaaa99e1e0a25f701ad9ef3e52cae2e762e3dd
anil <anilsangundi101@gmail.com>
2023-11-06 13:35:46 +0330

    second

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,5 @@
 import unittest
-from calculator import Calculator
+from FCalculator import Calculator
 
 class CalculatorTest(unittest.TestCase):
 
@@ -7,7 +7,7 @@ class CalculatorTest(unittest.TestCase):
         self.calc = Calculator()
 
     def test_add(self):
-        self.assertEqual(10, self.calc.add(3, 7), "The addition is wrong")
+        self.assertEqual(10, self.calc.add(5, 7), "The addition is wrong")
 
     def test_subtract(self):
         self.assertEqual(12, self.calc.subtract(15, 3), "Subtraction is wrong")
